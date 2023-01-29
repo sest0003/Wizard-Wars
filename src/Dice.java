4080894aa50e0ecf8ea3ae4d896e8119171c8be6
Sebastian Stillesjö <sebastian.stillesjo@gmail.com>
2023-01-29 10:22:06 +0100

    Version 2 dice object

---
+++ src/Dice.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Dice {
+
+
+    private int dice = ThreadLocalRandom.current().nextInt(1, 7);
+
+    public Dice() {
+        this.dice = dice;
+    }
+
+
+
+    public int getDice() {
+        return dice;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
 
 
 
