66a486161832a7743167267d8a4e41ca2c2eb853
Sebastian Stillesjö <sebastian.stillesjo@gmail.com>
2023-01-29 23:11:37 +0100

    Version 5 player object wolf-battle done

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,26 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
 
+        int wolfs = 700;
+
         Scanner scanner = new Scanner(System.in);
 
         Dice d1 = new Dice();
         Dice d2 = new Dice();
+        Dice computer1 = new Dice();
+        Dice computer2 = new Dice();
+
 
         List<Player> players = new ArrayList<>();
 
-        players.add(new Player(200, 20, 50, 10,50, 10));
-        players.add(new Player(200, 20, 50, 10,50, 10));
+        players.add(new Player(200, 30, 50, 10, 60,5));
+        players.add(new Player(200, 20, 60, 10,50, 10));
+
+
+
+
+
+
 
         System.out.println();
         System.out.print("Number of wizards: ");
@@ -29,19 +40,107 @@ public class Main {
         else if (nop == 2) {
             Player player1 = players.get(0);
             Player player2 = players.get(1);
+
+            int sumOfP1 = player1.getKnowledge();
+            sumOfP1 += player1.getArmor();
+            sumOfP1 += player1.getGold();
+            sumOfP1 += player1.getAttack();
+            sumOfP1 += player1.getSpells();
+            sumOfP1 += player1.getTower();
+
+            int sumOfP2 = player2.getKnowledge();
+            sumOfP2 += player2.getArmor();
+            sumOfP2 += player2.getGold();
+            sumOfP2 += player2.getAttack();
+            sumOfP2 += player2.getSpells();
+            sumOfP2 += player2.getTower();
+
+
+            storyLineRedwoods();
+
+            System.out.println();
+
+            StoryLineTheWolfs();
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println("the wolfs attacks with " + wolfs + "\nPlayer1, cast you spell by entering: 1 ");
+                scanner.nextInt();
+
+                int sumOfDicesP1 = d1.getDice() + d2.getDice();
+
+                int somOfWolfs1 = computer1.getDice() + computer2.getDice();
+
+                System.out.println("Player 1 rolled an " + sumOfDicesP1 );
+                System.out.println();
+                System.out.println("The wolfs attacked with " + somOfWolfs1);
+                System.out.println();
+
+
+                System.out.println("Player 2, Attack! : ");
+                scanner.nextInt();
+
+                int sumOfDicesP2 = d1.getDice() + d2.getDice();
+
+                int somOfWolfs2 = computer1.getDice() + computer2.getDice();
+
+                System.out.println("Player 2 rolled an " + sumOfDicesP2 );
+                System.out.println();
+                System.out.println("The wolfs attacked with " + somOfWolfs2);
+                System.out.println();
+
+               int sumWolfsTotal = somOfWolfs1 + somOfWolfs2 + wolfs;
+               int sumOfPlayerTotal = sumOfDicesP1 + sumOfDicesP2 + sumOfP1 + sumOfP2;
+
+                if (sumOfPlayerTotal > sumWolfsTotal) {
+                    System.out.println("The wolfs are slayed!!\n Wolfs : " + sumWolfsTotal + "\nYou: " + sumOfPlayerTotal);
+
+                }
+            }
+
+            else if (choice == 2) {
+               int newArmourP1 = player1.getArmor() - 10;
+               int newArmourP2 = player2.getArmor() - 10;
+                player1.setArmor(newArmourP1);
+                player2.setArmor(newArmourP2);
+                System.out.println("Detour!\n -10 in Armour");
+            }
+            System.out.println(player1);
+            System.out.println(player2);
+
         }
 
 
-        // storyLine();
 
 
 
 
+
+
+
+
+
+
+
+
+        }
+
+    private static void storyLineRedwoods() {
+        System.out.println();
+        System.out.println("^*^* The battle of Redwoods ^*^* ");
     }
+    private static void StoryLineTheWolfs() {
+        System.out.println();
 
-    private static void storyLine() {
-        System.out.println("*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^*^");
-        System.out.println("On your way to the battle you pass a village where you kan rest and prepare");
+        System.out.println("On the way to the Redwoods you run in to a pack of wolfs, do you try to kill them or take ");
+        System.out.println("detour and arrive to the battle with -10 in Armor? , attack enter 1 / detour enter 2: ");
     }
 
 }
+
+
+
+
+
+
